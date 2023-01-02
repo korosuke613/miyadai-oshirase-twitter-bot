abd6eccb8463b4f906fb3378ba5f4b97f33b6b28
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-01-02 21:28:59 +0900

    ci: setup

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,3 +1,8 @@
 export { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
 export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
-export { beforeEach, afterEach, describe, it } from "https://deno.land/std@0.170.0/testing/bdd.ts";
+export {
+  afterEach,
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.170.0/testing/bdd.ts";
--- src/__tests__/scraper.test.ts
+++ src/__tests__/scraper.test.ts
@@ -35,43 +35,43 @@ describe("Scraper", () => {
           "令和５年度博物館実習の説明会（オンデマンド）についてのお知らせ ",
         "url":
           "file:///gakumu/educationalinfo/educationalinfo/5637-2022-12-15.html",
-        "date": new Date("2022-12-14T15:00:00.000Z"),
+        "date": new Date("2022-12-15T00:00:00.000Z"),
       },
       {
         "title":
           "【緊急：全学生対象】12月19日以降「わかば」への学外からの接続方法が変わります",
         "url": "file:///gakumu/andsoon/andsoon/5634-2022-01-25-04-20-13.html",
-        "date": new Date("2022-12-08T15:00:00.000Z"),
+        "date": new Date("2022-12-09T00:00:00.000Z"),
       },
       {
         "title": "宮崎県知事選挙における期日前投票所の開設について",
         "url": "file:///gakumu/andsoon/andsoon/5626-2022-12-05-23-39-07.html",
-        "date": new Date("2022-12-05T15:00:00.000Z"),
+        "date": new Date("2022-12-06T00:00:00.000Z"),
       },
       {
         "title":
           "【案内】日本学生支援機構貸与奨学金における修士課程の返還免除内定制度について",
         "url":
           "file:///gakumu/campuslifeinfo/campuslifeinfo/5622-2022-11-30-08-01-29.html",
-        "date": new Date("2022-12-12T15:00:00.000Z"),
+        "date": new Date("2022-12-13T00:00:00.000Z"),
       },
       {
         "title":
           "【全学生対象】マイナンバーカードの取得状況調査について（依頼） (締切延長)",
         "url": "file:///gakumu/andsoon/andsoon/5628-2022-01-25-04-20-12.html",
-        "date": new Date("2022-12-05T15:00:00.000Z"),
+        "date": new Date("2022-12-06T00:00:00.000Z"),
       },
       {
         "title": "【重要】令和5年度前期授業料免除について　※旧制度",
         "url": "file:///gakumu/andsoon/andsoon/4992-2020-12-25.html",
-        "date": new Date("2022-12-11T15:00:00.000Z"),
+        "date": new Date("2022-12-12T00:00:00.000Z"),
       },
       {
         "title":
           "令和4年度後期入学料免除・入学料徴収猶予・授業料免除の結果について",
         "url":
           "file:///gakumu/campuslifeinfo/campuslifeinfo/4983-2020-12-10-05-50-50.html",
-        "date": new Date("2022-12-06T15:00:00.000Z"),
+        "date": new Date("2022-12-07T00:00:00.000Z"),
       },
     ]);
   });
